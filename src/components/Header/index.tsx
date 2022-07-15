e104fce7f8a5f4c0904aa8883d00d11dab9cca68
joaovitors1g <joao_vitor9846@hotmail.com>
2022-07-14 22:03:00 -0300

    feat: add click handler to new transaction button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,12 +2,18 @@ import { Container, Content } from './styles';
 
 import logo from '../../assets/logo.svg';
 
-export function Header() {
+type HeaderProps = {
+  openNewTransactionModal: () => void;
+};
+
+export function Header({ openNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logo} alt='DT Money' />
-        <button type='button'>Nova transação</button>
+        <button type='button' onClick={openNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
